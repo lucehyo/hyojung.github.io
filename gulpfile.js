107313fa388d29d59bc8e77fe60d6b5072c97537
lucehyo <zmgmgm123@gmail.com>
2023-03-07 19:06:57 +0900

    셋팅

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+var gulp = require('gulp');
+//var sass = require('gulp-sass');
+var fileinclude = require('gulp-file-include');
 
-exports.default = defaultTask
+
+gulp.task('fileinclude', function() {
+    gulp.src(['./2023portfolio/index.html', './2023portfolio/src/assets/html/*.html'], {base : './'})
+    .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+    }))
+    .pipe(gulp.dest('./'));
+});
+
