ee99632240daeb5c38ae7435958ae47ab3f20ad1
lucehyo <zmgmgm123@gmail.com>
2023-04-28 19:05:15 +0900

    js 변경

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -4,17 +4,6 @@ window.onload = function () {
   gsap.to('.txt-box .txt1', { y: 0, opacity: 1, duration: 0.5, delay: 0.35,addClass:"active"});
   gsap.to('.txt-box .name', { y: 0, opacity: 1, duration: 0.5, delay: 0.45});
   gsap.to('.txt-box .txt2', { y: 0, opacity: 1, duration: 0.5, delay: 0.55 });
- /* gsap.to("#home", {
-      scrollTrigger: {
-      trigger: "#home",
-      scrub: true,
-      pin: true,
-      start: "top top",
-      end: "bottom center",
-      toggleClass: "active",
-      ease: "power2"
-    }
-  });*/
 
 
   //gnb 스크롤
@@ -39,7 +28,7 @@ window.onload = function () {
     })
   })
 
-  //profile
+  //profile 배경 변경
   let timeline = function () {
     return gsap.timeline().fromTo(
       '.bg-wrap .bg-1',
@@ -65,8 +54,10 @@ window.onload = function () {
     markers:true,
   });
 
-  var about = document.querySelector('.about');
-  gsap.set('.about', { zIndex: (i, target, targets) => targets.length - i });
+
+  
+  //var about = document.querySelector('.about');
+  //gsap.set('.about', { zIndex: (i, target, targets) => targets.length - i });
   let timeline2 = function () {
     return gsap.timeline().fromTo(
       '.about',
@@ -90,10 +81,10 @@ window.onload = function () {
   });
 
   //about
-  let timeline4 = function () {
+  /*let timeline4 = function () {
     return gsap
       .timeline()
-      .set('.portfolio', {
+      .set('.about', {
         willChange: 'transform',
       })
       .fromTo(
@@ -114,58 +105,43 @@ window.onload = function () {
       });
   };
   ScrollTrigger.create({
-    trigger: '.portfolio',
+    trigger: '.about',
     animation: timeline4(),
     scrub: !0,
     start: 'top bottom',
-    end: () => `+=${about.clientHeight}`,
+    //end: () => `+=${about.clientHeight}`,
   }),
-  ScrollTrigger.create({
+  /*ScrollTrigger.create({
     trigger: '.portfolio',
     scrub: !0,
     pin: true,
     start: 'top top',
     end: '100%',
-  });
+  });*/
 
-  //section 넘기기
-  /*gsap.to("section:not(:last-child)", {
-    yPercent: -100, 
-    ease: "none",
-    stagger: 0.5,
-    scrollTrigger: {
-      trigger: "#container",
-      start: "top top",
-      end: "bottom bottom",
-      markers:true,
-      scrub: true,
-      pin: true
-    }
-  });
   
-  
-  gsap.set("section", {zIndex: (i, target, targets) => targets.length - i});*/
+  /*gsap.set("section", {zIndex: (i, target, targets) => targets.length - i});*/
 
 
   /* btn-top*/
 	$(".btn-top").hide();
 	$(function () {
-	  $(window).scroll(function () {
-		  if ($(this).scrollTop() > 100) {
-			  $('.btn-top').fadeIn();
-		  } else {
-			  $('.btn-top').fadeOut();
-		  }
-		 });
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.btn-top').fadeIn();
+		} else {
+			$('.btn-top').fadeOut();
+		}
+		});
 
-	  $('.btn-top').click(function () {
-		  $('body,html').animate({
-			  scrollTop: 0
-		  }, 500);
-		  return false;
-		 });
-	 });
-   
+  $('.btn-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+    });
+  });
+  
 };
 
 /*function setTab(id) {
